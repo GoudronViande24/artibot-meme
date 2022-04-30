a52aa1f161f2f90340271f62bda05d61e69acbfc
Thomas Fournier <thomas@artivain.com>
2022-04-29 23:51:41 -0400

    Adapt to artibot v3

--- index.js
+++ index.js
@@ -1,39 +1,122 @@
-// Manifest file for meme module
+import Artibot, { Command, Module, TriggerGroup } from "artibot";
+import Localizer from "artibot-localizer";
+import { Message } from "discord.js";
+import https from "https";
+import axios from "axios";
+import path from "path";
+import { fileURLToPath } from "url";
+import { createRequire } from 'module';
 
-module.exports = {
-	manifest: {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-		manifestVersion: 1,
-		moduleVersion: "1.2.0",
+const require = createRequire(import.meta.url);
+const { version } = require('./package.json');
+
+/**
+ * Meme module for Artibot
+ * @author GoudronViande24
+ * @license MIT
+ * @param {Artibot} artibot
+ * @returns {Module}
+ */
+export default ({ config: { lang } }) => {
+	localizer.setLocale(lang);
+
+	return new Module({
+		id: "meme",
 		name: "Meme",
-		supportedLocales: [
-			"en",
-			"fr"
+		version,
+		langs: [
+			"fr",
+			"en"
 		],
+		repo: "GoudronViande24/artibot-meme",
 		parts: [
-			{
+			new TriggerGroup({
 				id: "69",
-				type: "trigger",
-				path: "69.js"
-			},
+				triggers: [" 69 "],
+				mainFunction: (message) => message.reply({ content: "Nice" })
+			}),
 
-			{
+			new TriggerGroup({
 				id: "sus",
-				type: "trigger",
-				path: "sus.js"
-			},
+				triggers: ["sus "],
+				mainFunction: (message) => message.reply({ content: "ඞ" })
+			}),
 
-			{
+			new Command({
 				id: "chucknorris",
-				type: "command",
-				path: "chucknorris.js"
-			},
+				name: "chucknorris",
+				description: localizer._("Tells a Chuck Norris joke."),
+				aliases: ["cn"],
+				mainFunction: chuckNorris
+			}),
 
-			{
+			new Command({
 				id: "dadjoke",
-				type: "command",
-				path: "dadjoke.js"
-			}
+				name: "dadjoke",
+				description: localizer._("Tells a dad joke."),
+				mainFunction: dadJoke
+			})
 		]
-	}
-};
+	});
+}
+
+const localizer = new Localizer({
+	filePath: path.join(__dirname, "locales.json")
+});
+
+/**
+ * Dad joke command
+ * @param {Message} message 
+ * @param {string[]} args 
+ * @param {Artibot} artibot 
+ */
+function dadJoke(message, args, { createEmbed }) {
+	const reponse = await axios(options);
+	const joke = reponse.data.joke;
+
+	let embed = createEmbed()
+		.setTitle(localizer._("Dad joke"))
+		.setDescription(joke);
+
+	await message.reply({
+		embeds: [embed]
+	});
+}
+
+/**
+ * Chuck norris command
+ * @param {Message} message 
+ * @param {string[]} args 
+ * @param {Artibot} artibot 
+ */
+function chuckNorris(message, args, { log, createEmbed }) {
+	const options = {
+		hostname: 'api.chucknorris.io',
+		port: 443,
+		path: '/jokes/random',
+		method: 'GET'
+	};
+
+	const req = https.request(options, res => {
+		res.on('data', data => {
+			let embed = createEmbed()
+				.setTitle(localizer._("Chuck Norris joke"))
+				.setDescription(JSON.parse(data).value)
+				.setThumbnail(JSON.parse(data).icon_url);
+
+			message.reply({
+				embeds: [embed]
+			});
+		});
+	});
+
+	req.on('error', error => {
+		message.reply(localizer._("An error occured while executing this command."));
+		log("Meme", error);
+	});
+
+	req.end();
+}
