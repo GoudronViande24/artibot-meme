0f2cac86d94b4077c86b95793f038571becde38c
Thomas Fournier <thomas@artivain.com>
2023-04-01 19:26:03 -0400

    Forgor to change the package.json path

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const require = createRequire(import.meta.url);
-const { version } = require('./package.json');
+const { version } = require('../package.json');
 
 /**
  * Meme module for Artibot
